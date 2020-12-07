ec86cb91df7605e052d5a1ddd7264a70c2a9b39c
PC-DOS <suicunexp@outlook.com>
2020-12-08 01:54:33 +0800

    Update framework version to 4.5

--- Derpibooru Picture Saver/MainWindow.xaml.vb
+++ Derpibooru Picture Saver/MainWindow.xaml.vb
@@ -459,6 +459,7 @@ Class MainWindow
     End Sub
 
     Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 Or SecurityProtocolType.Tls Or SecurityProtocolType.Tls11 Or SecurityProtocolType.Tls12
         If chkUseTrixieBooru.IsChecked Then
             CurrentSearchPrefix = DerpibooruSearchPrefixBackup
         Else
